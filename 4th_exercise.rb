cabad9ad4facb8e5412d657c5f7bfe984efd4bfb
Ammad Ahmed <ammad.ahmed@pikessoft.com>
2015-10-09 16:26:42 +0500

    4th & 5th Excercise

---
+++ 4th_exercise.rb
@@ -0,0 +1,15 @@
+#! usr/bin/ruby -w
+
+# Initializing array 
+
+own_array =  [1,2,3,4,5,6,7,8,9,10]
+
+# Appending 11 at the end of the array 
+
+own_array.push(11)
+
+# Prepending 0 to the array 
+
+own_array.unshift(0)
+
+puts own_array
---
+++ 5th_exercise.rb
@@ -0,0 +1,17 @@
+#! usr/bin/ruby -w
+
+# Initializing array to iterate over thourgh ranger
+
+own_array = [0,1,2,3,4,5,6,7,8,9,10,11]
+
+# Poping out 11 from the end of the array 
+
+own_array.pop
+
+# pushing 3 at the end
+
+own_array<<(3)
+
+# printing array
+
+puts own_array
